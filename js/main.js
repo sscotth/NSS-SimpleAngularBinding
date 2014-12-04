d5f6c8cb6ae70c743c81c1a343dc2a07f50612a7
Scott <scott@sscotth.io>
2014-12-04 14:46:59 -0600

    hardcode sample data

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@
   angular.module('todoApp', [])
     .controller('TodoController', function(){
       var vm = this;
+      vm.tasks = [
+        {
+          name: 'Learn Angular',
+          desc: 'If I could learn Angular I\'d be sooooo happy!',
+          due: 'Today'
+        },
+        {
+          name: 'Finish Tic Tac Toe',
+          desc: 'Firebase Arrrrggggghhhhh!',
+          due: 'Monday'
+        },
+        {
+          name: 'Get a job',
+          desc: 'Profit!',
+          due: 'April 2015'
+        }
+      ]
     });
 
 }());
