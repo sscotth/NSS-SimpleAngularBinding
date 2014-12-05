94665f43344914a5152ef74c0ea3bff3c7d85fb3
Scott <scott@sscotth.io>
2014-12-05 10:08:27 -0600

    add Priority select box

--- js/main.js
+++ js/main.js
@@ -8,28 +8,39 @@
         {
           name: 'Learn Angular',
           desc: 'If I could learn Angular I\'d be sooooo happy!',
-          due: 'Today'
+          due: 'Today',
+          priority: 'high'
         },
         {
           name: 'Finish Tic Tac Toe',
           desc: 'Firebase Arrrrggggghhhhh!',
-          due: 'Monday'
+          due: 'Monday',
+          priority: 'medium'
         },
         {
           name: 'Get a job',
           desc: 'Profit!',
-          due: 'April 2015'
+          due: 'April 2015',
+          priority: 'medium'
         }
       ];
 
       vm.addNewTask = function(){
         vm.tasks.push(vm.newTask);
-        vm.newTask = null;
+        vm.newTask = _freshTask();
       };
 
       vm.removeTodo = function(todo){
         var index = vm.tasks.indexOf(todo);
         vm.tasks.splice(index,1);
+      };
+
+      vm.newTask = _freshTask();
+
+      function _freshTask(){
+        return {
+          priority: 'low'
+        }
       }
 
     });
