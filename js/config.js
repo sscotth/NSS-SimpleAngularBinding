b78eb3ecbe76c93978f3719299d6a63295a817db
Scott <scott@sscotth.io>
2014-12-11 13:19:40 -0600

    refactor routes to be more RESTful

--- js/config.js
+++ js/config.js
@@ -5,21 +5,24 @@
     .config(function($routeProvider){
       $routeProvider
       .when('/', {
+        templateUrl: 'views/landing.html'
+      })
+      .when('/todos', {
         templateUrl: 'views/table.html',
         controller: 'TodoController',
         controllerAs: 'todo'
       })
-      .when('/new', {
+      .when('/todos/new', {
         templateUrl: 'views/form.html',
         controller: 'TodoController',
         controllerAs: 'todo'
       })
-      .when('/:id', {
+      .when('/todos/:id', {
         templateUrl: 'views/show.html',
         controller: 'ShowController',
         controllerAs: 'show'
       })
-      .when('/:id/edit', {
+      .when('/todos/:id/edit', {
         templateUrl: 'views/form.html',
         controller: 'EditController',
         controllerAs: 'todo'
