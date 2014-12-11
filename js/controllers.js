3d259131d501e66110fa1e9714e77270727b8845
Scott <scott@sscotth.io>
2014-12-11 15:19:37 -0600

    add forgot password functionality

--- js/controllers.js
+++ js/controllers.js
@@ -38,6 +38,19 @@
         });
       }
 
+      vm.forgotPassword = function(){
+        var ref = new Firebase('https://omgttt.firebaseio.com');
+
+        ref.resetPassword({
+            email : vm.email
+          }, function(error) {
+          if (error === null) {
+            console.log("Password reset email sent successfully");
+          } else {
+            console.log("Error sending password reset email:", error);
+          }
+        });
+      };
     })
     .controller('ShowController', function($routeParams, todoFactory){
       var vm = this;
