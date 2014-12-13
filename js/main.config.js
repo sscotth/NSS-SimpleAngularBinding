f51219ea9f48cf3e44f8dad08ad95314f63e335e
Scott <scott@sscotth.io>
2014-12-12 22:09:05 -0600

    Move todo routes to their own file

--- js/main.config.js
+++ js/main.config.js
@@ -7,30 +7,6 @@
       .when('/', {
         templateUrl: 'views/landing.html'
       })
-      .when('/todos', {
-        templateUrl: 'views/table.html',
-        controller: 'TodoController',
-        controllerAs: 'todo',
-        private: true
-      })
-      .when('/todos/new', {
-        templateUrl: 'views/form.html',
-        controller: 'TodoController',
-        controllerAs: 'todo',
-        private: true
-      })
-      .when('/todos/:id', {
-        templateUrl: 'views/show.html',
-        controller: 'ShowController',
-        controllerAs: 'show',
-        private: true
-      })
-      .when('/todos/:id/edit', {
-        templateUrl: 'views/form.html',
-        controller: 'EditController',
-        controllerAs: 'todo',
-        private: true
-      })
       .otherwise({redirectTo: '/'});
     })
 }());
---
+++ js/todos/todo.config.js
@@ -0,0 +1,32 @@
+;(function(){
+  'use strict';
+
+  angular.module('todoApp')
+    .config(function($routeProvider){
+      $routeProvider
+      .when('/todos', {
+        templateUrl: 'views/table.html',
+        controller: 'TodoController',
+        controllerAs: 'todo',
+        private: true
+      })
+      .when('/todos/new', {
+        templateUrl: 'views/form.html',
+        controller: 'TodoController',
+        controllerAs: 'todo',
+        private: true
+      })
+      .when('/todos/:id', {
+        templateUrl: 'views/show.html',
+        controller: 'ShowController',
+        controllerAs: 'show',
+        private: true
+      })
+      .when('/todos/:id/edit', {
+        templateUrl: 'views/form.html',
+        controller: 'EditController',
+        controllerAs: 'todo',
+        private: true
+      })
+    })
+}());
