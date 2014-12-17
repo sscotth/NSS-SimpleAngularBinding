7a50a8b72d1705adb3edc61b24fa7b3446e7b515
Scott <scott@sscotth.io>
2014-12-17 12:42:35 -0600

    include ng-file-upload dependency

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 ;(function(){
   'use strict';
 
-  angular.module('todoApp', ['ngRoute', 'mgcrea.ngStrap'])
+  angular.module('todoApp', ['ngRoute', 'mgcrea.ngStrap', 'angularFileUpload'])
 
 }());
