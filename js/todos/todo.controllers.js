94a3af44f3c3936f5b415605012d9302f58dd85e
Scott <scott@sscotth.io>
2014-12-19 09:27:27 -0600

    intermediate

--- js/todos/todo.controllers.js
+++ js/todos/todo.controllers.js
@@ -2,38 +2,28 @@
   'use strict';
 
   angular.module('todoApp')
-    .controller('UploadController', function($rootScope, $scope, $upload, $routeParams, $location){
+    .controller('UploadController', function($rootScope, $scope, $routeParams){
       var vm = this,
           id = $routeParams.id;
 
-      vm.fileSelected = function(event){
-        _setThumbnail();
-      };
+      vm.imageUrl = 'https://omgttt-sscotth-io-images.s3.amazonaws.com/' + $rootScope.user.uid + '/' + id + '.jpg';
 
-      vm.uploadFiles = function(){
-        var file = vm.files[0];
-        $upload.upload({
-          url: 'https://omgttt-sscotth-io-images.s3.amazonaws.com',
-          method: 'POST',
-          data : {
-            'Content-Type' : file.type,
-            key: $rootScope.user.uid + '/' + id + '.jpg',
-            acl: 'public-read',
-            awsaccesskeyid: 'AKIAIGWKSR63NF7ZSRTA',
-            policy: 'eyJleHBpcmF0aW9uIjogIjIwMjAtMDEtMDFUMDA6MDA6MDBaIiwiY29uZGl0aW9ucyI6IFt7ImJ1Y2tldCI6ICJvbWd0dHQtc3Njb3R0aC1pby1pbWFnZXMifSx7ImFjbCI6ICJwdWJsaWMtcmVhZCJ9LFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1UeXBlIiwiIl0sWyJzdGFydHMtd2l0aCIsIiRrZXkiLCIiXV19',
-            signature: 'DsyFn+Gddc908PttZoeCYAxyrtg='
-          },
-          file: file
-        })
-        .success(function(){
-          $location.path('/todos')
+      vm.fileSelected = function(files){
+        _setThumbnail(files[0], function(base64){
+          $scope.$parent.todo.files[0].dataUrl = base64;
+          $scope.$apply();
         });
       };
 
-      function _setThumbnail () {
-        _imageToBase64(vm.files[0], function(base64){
-          vm.files[0].dataUrl = base64;
-          $scope.$apply();
+      vm.uploadFile = function(){
+        console.log('up')
+        var file = $scope.$parent.todo.files[0];
+        upload.uploadPhoto(file, $rootScope.user.uid, id);
+      };
+
+      function _setThumbnail (image, cb) {
+        _imageToBase64(image, function(data){
+          cb(data)
         });
       }
 
@@ -47,6 +37,30 @@
         }
       }
     })
+    .factory('uploadPhotoFactory', function($upload){
+      var factory = {}
+
+      factory.uploadPhoto = function(file, userId, todoId, cb){
+        $upload.upload({
+          url: 'https://omgttt-sscotth-io-images.s3.amazonaws.com',
+          method: 'POST',
+          data : {
+            'Content-Type' : file.type,
+            key: userId + '/' + todoId + '.jpg',
+            acl: 'public-read',
+            awsaccesskeyid: 'AKIAIGWKSR63NF7ZSRTA',
+            policy: 'eyJleHBpcmF0aW9uIjogIjIwMjAtMDEtMDFUMDA6MDA6MDBaIiwiY29uZGl0aW9ucyI6IFt7ImJ1Y2tldCI6ICJvbWd0dHQtc3Njb3R0aC1pby1pbWFnZXMifSx7ImFjbCI6ICJwdWJsaWMtcmVhZCJ9LFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1UeXBlIiwiIl0sWyJzdGFydHMtd2l0aCIsIiRrZXkiLCIiXV19',
+            signature: 'DsyFn+Gddc908PttZoeCYAxyrtg='
+          },
+          file: file
+        })
+        .success(function(){
+          typeof cb === 'function' && cb();
+        });
+      };
+
+      return factory;
+    })
     .controller('ShowController', function($routeParams, todoFactory){
       var vm = this;
       var id = $routeParams.id;
@@ -54,7 +68,7 @@
         vm.task = data;
       });
     })
-    .controller('EditController', function($routeParams, todoFactory){
+    .controller('EditController', function($rootScope, $routeParams, todoFactory, uploadPhotoFactory){
       var vm = this;
       var id = $routeParams.id;
 
@@ -80,6 +94,7 @@
         todoFactory.createTodo(vm.newTask, function(data){
           vm.tasks = vm.tasks || {};
           vm.tasks[data.name] = vm.newTask;
+          upload.addPhoto(vm.files[0].dataUrl)
           vm.newTask = _freshTask();
         });
       };
