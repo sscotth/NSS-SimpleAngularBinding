8d360dcd0a29c469f34e53449921a21d53199b48
Scott <scott@sscotth.io>
2014-12-19 11:51:42 -0600

    initial Gruntfile setup

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+  });
+
+  grunt.registerTask('default', []);
+};
